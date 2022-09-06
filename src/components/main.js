79b8eee811f235e85219c591ce5366cdd6228bef
Dada Oluwashina <dada.oluwashina@longbridgetech.com>
2022-09-06 04:20:24 +0100

    initialize react-router-dom to link pages

--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,18 @@
 import Header from './header';
 import Home from './home';
 import Footer from './footer';
+import About from './about';
+
+import {Routes as Switch, Route} from 'react-router-dom';
 
 function Main() {
   return (
     <div className="App">
       <Header/>
-      <Home/>
+      <Switch>
+         <Route path="/" element={<Home/>}/>
+         <Route path="/about" element={<About/>}/>
+      </Switch>
       <Footer/>
     </div>
   );
