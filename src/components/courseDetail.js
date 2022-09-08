2471bd46270de09c2b98458248cf2cce640d3e4a
Dada Oluwashina <dada.oluwashina@longbridgetech.com>
2022-09-08 01:42:01 +0100

    added modal and iframe for video purpose

--- src/components/courseDetail.js
+++ src/components/courseDetail.js
@@ -31,38 +31,59 @@ function CourseDetail(){
                         <li className="list-group-item">Introduction 
                             <span className="float-end">
                                 <span className="me-5">1 Hour 30 Minutes</span>
-                                 <button className="btn btn-sm btn-danger float-end"><i class="bi-youtube"></i></button>
+                                 <button className="btn btn-sm btn-danger float-end"><i className="bi-youtube"></i></button>
                             </span>
                         </li>
                         <li className="list-group-item">Introduction 
                             <span className="float-end">
                                  <span className="me-5">1 Hour 30 Minutes</span>
-                                 <button className="btn btn-sm btn-danger float-end"><i class="bi-youtube"></i></button>
+                                 <button className="btn btn-sm btn-danger float-end"><i className="bi-youtube"></i></button>
                             </span>
                         </li>
                         <li className="list-group-item">Introduction 
                             <span className="float-end">
                                  <span className="me-5">1 Hour 30 Minutes</span>
-                                 <button className="btn btn-sm btn-danger float-end"><i class="bi-youtube"></i></button>
+                                 <button className="btn btn-sm btn-danger float-end"><i className="bi-youtube"></i></button>
                             </span>
                         </li>
                         <li className="list-group-item">Introduction 
                             <span className="float-end">
                                  <span className="me-5">1 Hour 30 Minutes</span>
-                                 <button className="btn btn-sm btn-danger float-end"><i class="bi-youtube"></i></button>
+                                 <button className="btn btn-sm btn-danger float-end"><i className="bi-youtube"></i></button>
                             </span>
                         </li>
                         <li className="list-group-item">Introduction 
                             <span className="float-end">
                                  <span className="me-5">1 Hour 30 Minutes</span>
-                                 <button className="btn btn-sm btn-danger float-end"><i class="bi-youtube"></i></button>
+                                 <button className="btn btn-sm btn-danger float-end"><i className="bi-youtube"></i></button>
                             </span>
                         </li>
                         <li className="list-group-item">Introduction 
                             <span className="float-end">
                                  <span className="me-5">1 Hour 30 Minutes</span>
-                                 <button className="btn btn-sm btn-danger float-end"><i class="bi-youtube"></i></button>
+                                 <button className="btn btn-sm btn-danger float-end" data-bs-toggle="modal" data-bs-target="#videoModal1"><i className="bi-youtube"></i></button>
                             </span>
+                            {/* video modal start*/}
+
+                                <div className="modal fade" id="videoModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div className="modal-dialog modal-xl">
+                                    <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h5 className="modal-title" id="exampleModalLabel">Video 1</h5>
+                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div className="modal-body">
+                                        <div class="ratio ratio-16x9">
+                                            <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" allowfullscreen></iframe>
+                                            </div>
+                                        </div>
+                                   
+                                    </div>
+                                </div>
+                                </div>
+                                         
+                            {/* end video modal*/}
+                             
                         </li>
                         
                     </ul>
